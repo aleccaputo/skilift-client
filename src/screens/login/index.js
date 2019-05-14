8e22e6827689ff591e9b7f114d3d2f8f0371f506
Alec <acaputo@mail.smcvt.edu>
2019-05-13 20:29:09 -0400

    center

--- src/screens/login/index.js
+++ src/screens/login/index.js
@@ -3,6 +3,9 @@ import TextField from "@material-ui/core/TextField";
 import Button from '@material-ui/core/Button';
 import {login} from "./actions";
 import {connect} from 'react-redux';
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import makeStyles from "@material-ui/core/styles/makeStyles";
 
 const Login = ({dispatch, auth}) => {
     const [username, setUsername] = useState('');
@@ -10,28 +13,43 @@ const Login = ({dispatch, auth}) => {
     return (
         auth.token === null
             ?
-        <Fragment>
-            <TextField
-                label={'Username'}
-                value={username}
-                onChange={e => setUsername(e.target.value)}
-            />
-            <TextField
-                label={'Password'}
-                type={'password'}
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-            />
-            {
-                auth.error ? <p>{'Invalid login, please try again'}</p> : null
-            }
-            <Button variant={'contained'} color={'primary'} onClick={() => login(username, password, dispatch)}>Login</Button>
-        </Fragment>
+            <form>
+                <Grid
+                    container
+                    direction="column"
+                    justify="center"
+                    alignItems="center"
+                    spacing={2}
+                >
+                    <Grid item={true}>
+                        <TextField
+                            label={'Username'}
+                            value={username}
+                            onChange={e => setUsername(e.target.value)}
+                        />
+                    </Grid>
+                    <Grid item={true}>
+                        <TextField
+                            label={'Password'}
+                            type={'password'}
+                            value={password}
+                            onChange={e => setPassword(e.target.value)}
+                        />
+                    </Grid>
+                    {
+                        auth.error ? <p>{'Invalid login, please try again'}</p> : null
+                    }
+                    <Grid item={true}>
+                        <Button variant={'contained'} color={'primary'}
+                                onClick={() => login(username, password, dispatch)}>Login</Button>
+                    </Grid>
+                </Grid>
+            </form>
             :
             <p>{'You are already logged in'}</p>
     )
 };
 const mapStateToProps = (state) => ({
-   auth: state.auth
+    auth: state.auth
 });
 export default connect(mapStateToProps)(Login);
